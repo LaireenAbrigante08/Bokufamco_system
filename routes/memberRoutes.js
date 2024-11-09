5bbe2585d56924f25328065c7170f636a7423025
Jamaica Mistiola <161783797+Mistiola16@users.noreply.github.com>
2024-11-09 21:22:07 +0800

    Update and rename memberInformationRoutes.js to memberRoutes.js

---
+++ routes/memberRoutes.js
@@ -0,0 +1,26 @@
+// routes/memberRoutes.js
+
+const express = require('express');
+const router = express.Router();
+
+
+// Importing the controller with db connection
+module.exports = (db) => {
+    const memberController = require('../controllers/memberController')(db); 
+
+    // Route to render the member form (for creating a new member)
+    router.get('/', (req, res) => {
+        memberController.getMemberForm(req, res);
+    });
+
+    // Route to handle form submission and save member data
+    router.post('/save', (req, res) => {
+        memberController.saveMember(req, res);
+    });
+
+    router.post('/update', (req, res) => {
+        memberController.updateMember(req, res);
+    });
+
+    return router;
+};

fc75720d273317c6de28aa0eb831cf9fc854dcb3
Jamaica Mistiola <161783797+Mistiola16@users.noreply.github.com>
2024-11-09 16:53:50 +0800

    Update Member.js

--- models/Member.js
+++ models/Member.js
@@ -1,26 +1,34 @@
-const db = require('../config/db');
+module.exports = (db) => {
+    return {
+        // Fetch the first member data
+        getMember: (callback) => {
+            const query = 'SELECT * FROM members LIMIT 1'; // For simplicity, fetching the first member in the database
+            db.query(query, callback);  // Execute the query and pass the result to the callback function
+        },
 
-const Member = {
-    findById: async (id) => {
-        try {
-            const [member] = await db.query('SELECT * FROM members WHERE id = ?', [id]);
-            return member[0]; // Return single member or undefined if not found
-        } catch (error) {
-            throw error;
+        // Insert or update a member's data
+        updateMember: (data, callback) => {
+            const query = `
+                INSERT INTO members (first_name, middle_name, last_name, address, dob, email, gender, id_number, contact_number)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                ON DUPLICATE KEY UPDATE
+                    first_name = VALUES(first_name),
+                    middle_name = VALUES(middle_name),
+                    last_name = VALUES(last_name),
+                    address = VALUES(address),
+                    dob = VALUES(dob),
+                    email = VALUES(email),
+                    gender = VALUES(gender),
+                    id_number = VALUES(id_number),
+                    contact_number = VALUES(contact_number);
+            `;
+            
+            const values = [
+                data.first_name, data.middle_name, data.last_name, data.address, data.dob, data.email,
+                data.gender, data.id_number, data.contact_number
+            ];
+            
+            db.query(query, values, callback);  // Execute the query with the provided values
         }
-    },
-
-    updateMember: async (id, details) => {
-        const { lastname, firstname, middlename, dob, address, gender, contact_information, picture } = details;
-        try {
-            await db.query('UPDATE members SET lastname = ?, firstname = ?, middlename = ?, dob = ?, address = ?, gender = ?, contact_information = ?, picture = ? WHERE id = ?', 
-            [lastname, firstname, middlename, dob, address, gender, contact_information, picture, id]);
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    // Add more member-related methods here if needed
+    };
 };
-
-module.exports = Member;
